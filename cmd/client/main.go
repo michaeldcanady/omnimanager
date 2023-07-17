34401242aa8f6ecc1dd6db5f9db203325890f9a4
michaeldcanady <michaeldcanady+github@gmail.com>
2023-07-17 17:49:19 -0400

    adding service

---
+++ cmd/client/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Microsoft/go-winio"
+)
+
+func main() {
+	pipePath := `\\.\pipe\your_pipe_name`
+	f, err := winio.DialPipe(pipePath, nil)
+	if err != nil {
+		log.Fatalf("error opening pipe: %v", err)
+	}
+	defer f.Close()
+	n, err := f.Write([]byte("sync"))
+	if err != nil {
+		log.Fatalf("write error: %v", err)
+	}
+	log.Println("wrote:", n)
+}
